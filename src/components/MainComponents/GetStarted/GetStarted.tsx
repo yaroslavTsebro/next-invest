d5acc1c8abf537e38b47bc7e144cadfa2f285d8b
Yaroslav Tsebro <yaroslav.cebro@gmail.com>
2023-08-29 14:33:28 +0300

    added get-started block

--- src/components/MainComponents/GetStarted/GetStarted.tsx
+++ src/components/MainComponents/GetStarted/GetStarted.tsx
@@ -1,3 +1,21 @@
+import "./GetStarted.scss";
+
 export function GetStarted() {
-  return <></>
+  return (
+    <section className="get-started">
+      <div className="get-started__container">
+        <div className="get-started__body get-started-info">
+          <div className="get-started-info__title">
+            Meaningful investments in Main Street businesses
+          </div>
+          <div className="get-started-info__text">
+            Browse vetted investment offerings in communities all over the US.
+          </div>
+          <button className="get-started-info__button button button_purple">
+            Get Started
+          </button>
+        </div>
+      </div>
+    </section>
+  );
 }
--- src/components/MainComponents/Main/Main.tsx
+++ src/components/MainComponents/Main/Main.tsx
@@ -1,5 +1,10 @@
+import { GetStarted } from "../GetStarted/GetStarted";
 import "./Main.scss";
 
 export function Main() {
-  return <main className="main"></main>;
+  return (
+    <main className="main">
+      <GetStarted />
+    </main>
+  );
 }
