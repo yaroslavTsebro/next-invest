7c5c0cda582d44a9519373d0885578aad4d935d3
Yaroslav Tsebro <yaroslav.cebro@gmail.com>
2023-08-30 14:31:05 +0300

    added footer

--- src/components/FooterComponents/Footer/Footer.tsx
+++ src/components/FooterComponents/Footer/Footer.tsx
@@ -1,5 +1,20 @@
 import "./Footer.scss";
 
 export function Footer() {
-  return <>Footer</>;
+  return (
+    <footer className="footer">
+      <div className="footer__container">
+        <div className="footer__upper upper-footer">
+          <div className="upper-footer__body"></div>
+          <div className="upper-footer__button"></div>
+        </div>
+        <div className="footer__lower lower-footer">
+          <div className="lower-footer__subscribe">
+            <div className="lower-footer-subscribe__title">Subscribe to our newsletter</div>
+          </div>
+          <div className="lower-footer__social"></div>
+        </div>
+      </div>
+    </footer>
+  );
 }
